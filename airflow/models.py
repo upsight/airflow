ca5180b4b51f5a74ffd0c5adc3f4bdb7619e7b17
Gregory Benison <gbenison@comscore.com>
2017-09-16 17:21:31 +0200

    [AIRFLOW-1614] Replace inspect.stack() with sys._getframe()
    inspect.stack() is really expensive, and slows
    down processing of dags
    having large numbers (100s, 1000s) of subdags.
    
    Closes #2610 from gbenison/gcbenison2

--- airflow/models.py
+++ airflow/models.py
@@ -30,7 +30,6 @@ import getpass
 import imp
 import importlib
 import itertools
-import inspect
 import zipfile
 import jinja2
 import json
@@ -2749,7 +2748,7 @@ class DAG(BaseDag, LoggingMixin):
 
         self._description = description
         # set file location to caller source path
-        self.fileloc = inspect.getsourcefile(inspect.stack()[1][0])
+        self.fileloc = sys._getframe().f_back.f_code.co_filename
         self.task_dict = dict()
         self.start_date = start_date
         self.end_date = end_date
